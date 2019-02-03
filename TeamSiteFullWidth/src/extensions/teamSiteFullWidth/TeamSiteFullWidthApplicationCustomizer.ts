2da82d73d75ece7f0ece6d6fad28ed166921ccd5
João Ferreira <joao12ferreira@gmail.com>
2019-02-03 18:45:01 +0000

    Add files via upload
    Enable/Disable fullwidth header zone

--- TeamSiteFullWidth/src/extensions/teamSiteFullWidth/TeamSiteFullWidthApplicationCustomizer.ts
+++ TeamSiteFullWidth/src/extensions/teamSiteFullWidth/TeamSiteFullWidthApplicationCustomizer.ts
@@ -1,6 +1,7 @@
 import { override } from '@microsoft/decorators';
 import { BaseApplicationCustomizer } from '@microsoft/sp-application-base';
 export interface ITeamSiteFullWidthApplicationCustomizerProperties {
+  fullWidthZone: boolean;
 }
 
 require("./css/customStyles.module.scss");
@@ -17,6 +18,89 @@ export default class TeamSiteFullWidthApplicationCustomizer extends BaseApplicat
         resizeEvent.initUIEvent('resize', true, false, window, 0);
         window.dispatchEvent(resizeEvent);
       }
+
+      let fullWidthZone: boolean = this.properties.fullWidthZone;
+      var style : string;
+
+      if (fullWidthZone == false) {
+        // don't create fullwidth zone
+
+        style=`.sp-pageLayout-sideNav [class^='deferredLeftNav'] {
+          max-width: 0px;
+        }
+
+        [class^='pageHeader'] {
+          display: none;
+        }        
+      
+        .sp-pageLayout-sideNav .SPCanvas-canvas  {
+          max-width: 100%;
+        }      
+                     
+        .sp-pageLayout-sideNav .CanvasZone.row.CanvasZone--alignment.CanvasZone--read.CanvasZone--noMargin {
+          margin: auto;
+        }
+      
+        .sp-pageLayout-sideNav [class^='spInnerCommentsWrapper_'] {
+          width: 100%;
+          margin-left: auto;
+          margin-right: auto;
+        }`;       
+      } else {  
+        style=`.sp-pageLayout-sideNav [class^='deferredLeftNav'] {
+          max-width: 0px;
+        }
+
+        [class^='pageHeader'] {
+          display: none;
+        }        
+      
+        .sp-pageLayout-sideNav .SPCanvas-canvas  {
+          max-width: 100%;
+        }
+      
+        .sp-pageLayout-sideNav .CanvasZoneContainer:first-child .CanvasZone.row {
+          max-width: 100%;
+          padding: 0;
+        }
+
+        .sp-pageLayout-sideNav .CanvasZoneContainer:first-child .CanvasZone.row .CanvasSection {
+          padding: 0;
+        }
+
+        .sp-pageLayout-sideNav .CanvasZoneContainer:first-child .CanvasZone.row .CanvasSection .ControlZone {
+          padding: 0;
+          margin: 0;
+        }
+      
+        .sp-pageLayout-sideNav .CanvasZoneContainer:first-child .CanvasZone.row,
+        .sp-pageLayout-sideNav .CanvasZoneContainer:first-child .CanvasZone.row .CanvasSection,
+        .sp-pageLayout-sideNav .CanvasZoneContainer:first-child .CanvasZone.row .CanvasSection .ControlZone,
+        {
+          margin: 0px;
+          padding: 0px;
+        }
+      
+        .sp-pageLayout-sideNav .CanvasZone.row.CanvasZone--alignment.CanvasZone--read.CanvasZone--noMargin {
+          margin: auto;
+        }
+      
+        .sp-pageLayout-sideNav [class^='spInnerCommentsWrapper_'] {
+          width: 100%;
+          margin-left: auto;
+          margin-right: auto;
+        }`;
+      }
+
+      var css = style;
+      var head = document.head || document.getElementsByTagName('head')[0];
+      var styletag = document.createElement('style');
+
+      styletag.type = 'text/css';
+      styletag.appendChild(document.createTextNode(css));      
+
+      head.appendChild(styletag);
+
     return Promise.resolve();
   }
 }
---
+++ TeamSiteFullWidth/src/extensions/teamSiteFullWidth/css/customStyles.module.scss.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+require("./customStyles.module.css");
+const styles = {
+};
+
+export default styles;
+/* tslint:enable */
